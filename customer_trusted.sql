7783a30a88461980560879c173793f2d7ee6b634
GhaziSyed <115798228+GhaziSyed@users.noreply.github.com>
2023-06-18 20:55:06 +0200

    Create customer_trusted.sql

---
+++ customer_trusted.sql
@@ -0,0 +1,21 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `albis`.`customer_trusted` (
+  `serialnumber` string,
+  `sharewithpublicasofdate` bigint,
+  `birthday` string,
+  `registrationdate` string,
+  `customername` string,
+  `email` string,
+  `lastupdatedate` bigint,
+  `phone` string,
+  `sharewithfriendsasofdate` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://albis-lake-house/customer/trusted/'
+TBLPROPERTIES ('classification' = 'json');
